86c9afa0baa258ef8bdb44a864401cf7b4c0b044
Uri_Klevcov <a-08-137.mail.ru>
2022-10-12 00:44:38 +0600

    Example 005 If Else

---
+++ Examples/Example005_IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя  ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "даша")
+{
+    Console.WriteLine("Да не уж то наша Даша почтила нас своим присутствием!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+
