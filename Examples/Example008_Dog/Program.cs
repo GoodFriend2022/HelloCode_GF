40aa02ffecf615e5f00026e1c7cc4ceb0046ca47
Uri_Klevcov <a-08-137.mail.ru>
2022-10-13 21:21:14 +0600

    Example 008 Dog

---
+++ Examples/Example008_Dog/Program.cs
@@ -0,0 +1,29 @@
+﻿double firstFriendSpeed = new Random().Next(1, 11);
+double secondFriendSpeed = new Random().Next(1, 11);
+double dogSpeed = new Random().Next(10, 16);
+double distants = 1500;
+int friend = 1;
+int minDistants = 10;
+int count = 0;
+
+Console.WriteLine("Скорость первого друга равна " + firstFriendSpeed + ", скорость второго друга - " + secondFriendSpeed + ", скорость собаки - " + dogSpeed);
+
+while (minDistants < distants)
+{
+    if (friend == 1)
+    {
+        distants = distants - (firstFriendSpeed + secondFriendSpeed) * distants / (secondFriendSpeed + dogSpeed);
+        friend = 2;
+        count++;
+    }
+    else
+    {
+        distants = distants - (firstFriendSpeed + secondFriendSpeed) * distants /(firstFriendSpeed + dogSpeed);
+        friend = 1;
+        count++;
+    }
+}
+
+Console.WriteLine("От одного друга до другого собака пробежит " + count + " раз");
+
+
